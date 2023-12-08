216ced9691f969ee6b15493f6a6402cac2e08be0
Joan David Frent Frent <joandavid.frentf@estudiants.urv.cat>
2023-12-08 12:08:22 +0100

    Comentarios

--- src/reservations/ListReservations.java
+++ src/reservations/ListReservations.java
@@ -1,13 +1,10 @@
+package reservations;
 /** Class listReservations.java
  * 
- * This class is made for storing a list of bookings
- * 
- * It will also update the number of assistants that can take part of 
- *  until it's full
+ * This class is made for storing a list of reservations
  * 
  * @author Joan David Frent Frent (joandavid.frentf@estudiants.urv.cat)
  */
-package reservations;
 
 public class ListReservations {
 
