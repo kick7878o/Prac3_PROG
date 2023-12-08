bbc044093bd9e9f9e81b00a608f609a453a5090c
Joan David Frent Frent <joandavid.frentf@estudiants.urv.cat>
2023-12-08 12:07:52 +0100

    Rehecho de nuevo el paquete de reservas

---
+++ src/reservations/ListReservations.java
@@ -0,0 +1,14 @@
+/** Class listReservations.java
+ * 
+ * This class is made for storing a list of bookings
+ * 
+ * It will also update the number of assistants that can take part of 
+ *  until it's full
+ * 
+ * @author Joan David Frent Frent (joandavid.frentf@estudiants.urv.cat)
+ */
+package reservations;
+
+public class ListReservations {
+
+}
---
+++ src/reservations/Reservation.java
@@ -0,0 +1,11 @@
+package reservations;
+/** Class reservation.java
+ * 
+ * This class is made for storing one reservation
+ *  that the user made to take part of the workshop at some given
+ *  hour and duration of the day.
+ */
+// package User
+public class Reservation {
+   
+}
