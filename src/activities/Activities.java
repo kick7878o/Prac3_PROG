c068f8b495cae8871a56795f3e1e3bd1a70e7724
davidlopezromero <73308607+davidlopezromero@users.noreply.github.com>
2023-12-30 15:58:30 +0000

    30/12/23 David L. v1

--- src/activities/Activities.java
+++ src/activities/Activities.java
@@ -19,7 +19,30 @@ public abstract class Activities {
     protected int activityDay; // Day that it will be taking place (10-19)
     protected String entityCreator; // Name of the entity that created the Activity
 
-    /** Contructor of the activities 
+    /** Contructor of the activities used
+     * for when a new activity is created (new code generation)
+     * 
+     * @param actType type of the activity
+     * @param act_code code of the activity
+     * @param act_name name of the activity
+     * @param act_location location of the activity
+     * @param act_postalCode postal code of the activity
+     * @param act_day day that it'll take place
+     * @param entityCreator entitiy that created the activity
+    */
+    public Activities(ActivityType actType, String act_name, String act_location,
+                      int act_postalCode, int act_day, String entityCreator) {
+        this.actType = actType;
+        this.activityCode = generateActivityCode();
+        this.activityName = act_name;
+        this.activityLocation = act_location;
+        this.postalCode = act_postalCode;
+        this.activityDay = act_day;
+        this.entityCreator = entityCreator;
+    }
+
+    /** Contructor of the activities used
+     * for when an activity was already created (without code generation)
      * 
      * @param actType type of the activity
      * @param act_code code of the activity
@@ -43,6 +66,7 @@ public abstract class Activities {
     // Getters & Setters for the attributes
     public ActivityType getActType() { return actType; }
     public String getActivityCode() { return activityCode; }
+    public void setActivityCode(String activityCode) { this.activityCode = activityCode; }
     public String getActivityName() { return activityName; }
     public void setActivityName(String activityName) { this.activityName = activityName; }
     public String getActivityLocation() { return activityLocation; }
@@ -52,16 +76,25 @@ public abstract class Activities {
     public int getActivityDay() { return activityDay; }
     public void setActivityDay(int activityDay) { this.activityDay = activityDay; }
     public String getEntityCreator() { return entityCreator; }
+    public void setEntityCreator(String entityCreator) { this.entityCreator = entityCreator; }
+
+    public String generateActivityCode() {
+        // New substring with their first 3 letters: URVASB -> aux = URV
+        String aux = entityCreator.substring(0,2).toUpperCase();
+        // Generate random number between 100 & 900
+        int num = 100 + (int) (Math.random() * 900);
+
+        return activityCode = aux + num;
+    }
     
     @Override
     public String toString() {
-        return "Activity type: " +actType+ "\n" +
-               "Activity code: " +activityCode+ "\n" +
-               "Activity name: " +activityName+ "\n" +
-               "Activity location: " +activityLocation+ "\n" +
-               "Postal code: " +postalCode+ "\n" +
-               "Activity day: " +activityDay+ "\n" +
-               "Entity creator: " +entityCreator+ "\n";
+        return "Activity code: " + activityCode + "\n" +
+               "Activity name: " + activityName + "\n" +
+               "Activity location: " + activityLocation + "\n" +
+               "Postal code: " + postalCode + "\n" +
+               "Activity day: " + activityDay + "\n" +
+               "Entity creator: " + entityCreator + "\n";
     }
 
     // Abstract method to copy an instance
@@ -77,14 +110,19 @@ public abstract class Activities {
      * @param txt string with data
      */
     public abstract void fromTextFormat(String txt);
-    
-    public boolean isAudioGuided() { 
-        Activities a = new Visits(isAudioGuided(), isAudioGuided(), activityDay, activityName, activityLocation, postalCode, activityCode, entityCreator);
-        return a.isAudioGuided(); 
-    }
 
+
+    /** Method that calls a Visits function to use in Activities
+     * @return boolean with data
+     */
+    public boolean isAudioGuided() { 
+        Activities a = new Visits(isAudioGuided(), isAudioGuided(), activityDay, activityName, activityLocation, activityCode, postalCode, activityCode, entityCreator);
+        return a.isAudioGuided(); }
+
+    /** Method that calls a Visits function to use in Activities
+     * @return boolean with data
+     */
     public boolean isBlindFriendly() { 
-        Activities a = new Visits(isAudioGuided(), isAudioGuided(), activityDay, activityName, activityLocation, postalCode, activityCode, entityCreator);
-        return a.isBlindFriendly(); 
-    }
+    Activities a = new Visits(isAudioGuided(), isAudioGuided(), activityDay, activityName, activityLocation, activityCode, postalCode, activityCode, entityCreator);
+    return a.isBlindFriendly(); }
 }
--- src/activities/ListOfActivities.java
+++ src/activities/ListOfActivities.java
@@ -43,7 +43,7 @@ public class ListOfActivities {
    /** Method that adds an activity to the list of activities
     * 
     * NOTE: resizes if the list is full
-    * @param actv an insatnce of activity
+    * @param actv an instance of activity
     */
    public void addActivity(Activities actv) {
       // Resize the array if necessary
@@ -59,26 +59,31 @@ public class ListOfActivities {
       listActv[nElem] = actv.copy();
       nElem++;
    }
+   /** Method that gets activities stored in an specific day
+    * 
+    * NOTE: it checks all the array to get the activities of this day
+    * @param day a day to check the activities of
+    */
+   public ListOfActivities getActivityByDay(int day){
+      // create an auxiliar to save the specified
+      ListOfActivities aux = new ListOfActivities(nElem);;
+      int j=0;
+      for(int i=0;i<listActv.length;i++){
+         if(listActv[i].activityDay == day){
+           aux.addActivity(listActv[j]);
+           j++;
+         }
+      }
+      return aux;
+   }
 
    @Override
    public String toString() {
       String aux = "List of Activities [" +(nElem+1)+ "] =>";
 
       for (int i=0; i < nElem; i++)
-         aux += "\n   [" +(i+1)+ "] " +listActv[i]+ "\n";
+         aux += "\n   [" +i+ "] " +listActv[i]+ "\n";
 
-      return aux;
-   }
-
-   public ListOfActivities getActivityByDay(Activities[] actv, int day){
-      ListOfActivities aux = new ListOfActivities(nElem);;
-      int j=0;
-      for(int i=0;i<actv.length;i++){
-         if(actv[i].activityDay == day){
-           aux.addActivity(listActv[j]);
-           j++;
-         }
-      }
       return aux;
    }
 
@@ -95,78 +100,93 @@ public class ListOfActivities {
       return aux;
    }
 
-      public void agefirVisita(Visits act){
-         if (nElem<listActv.length) {
-               for (int i = 0; i < nElem; i++) {
-                     if (act.getActivityName().charAt(0) <= 
-                        (listActv[i].getActivityName().charAt(0))) {
-                        for(int k=nElem; k>i; k--)
-                           listActv[k]=listActv[k-1];
-                        
-                        listActv[i]=act;
-                     }
-               } 
-         }
-   }
+   /** Method that adds a Visit to the list of activities
+    * 
+    * NOTE: it checks if the name is earlier in the periodic table to put it in order.
+    */
+   public void addVisit(Visits act){
+      if(nElem<listActv.length){
+              for (int i = 0; i < nElem; i++) {
+                  if(act.getActivityName().charAt(0)<=(listActv[i].getActivityName().charAt(0))){
+                      for(int k=nElem; k>i; k--){
+                          listActv[k]=listActv[k-1];
+                      }
+                      listActv[i]=act;
+                  }
+          } 
+      }
+  }
 
-   /**
-      * Removes a visit activity from the list based on its code.
-      *
-      * @param codi The code of the visit activity to be removed.
-      */
-   public void eliminarUsuari(String codi) {
-         int i = 0;
-         while (i < nElem)
-            if (listActv[i].getActivityCode().equalsIgnoreCase(codi)) {
-               for (int j = i; j < nElem - 1; j++)
-                     listActv[j] = listActv[j + 1];
-               
-               nElem--;
-            } else
-               i++;
-   }
+  /**
+   * Removes a visit activity from the list based on its code.
+   *
+   * @param codi The code of the visit activity to be removed.
+   */
+  public void deleteActivity(String codi) {
+      int i = 0;
+      while (i < nElem)
+          if (listActv[i].getActivityCode().equalsIgnoreCase(codi)) {
+              for (int j = i; j < nElem - 1; j++) {
+                  listActv[j] = listActv[j + 1];
+              }
+              nElem--;
+          } else
+              i++;
+  }
 
-   private ListOfActivities audioGuideFiltered(Entity entit, boolean audio){
-      int j=0;
+  /** Method that creates a new auxiliar table to save the audio guided activities
+    * 
+    * @return a new list with the audio guied activities
+    */
+  private ListOfActivities audioGuideFiltered(Entity entit, boolean audio){
+   int j=0;
 
-      ListOfActivities aux = new ListOfActivities(nElem);
-      if (audio==true) {
-         for (int i=0; i < listActv.length; i++) {
-            if(listActv[i].entityCreator == entit.getName() && 
-               listActv[i].isAudioGuided() == audio) {
-               aux.addActivity(listActv[j]);
-               j++;
-            }
+   ListOfActivities aux = new ListOfActivities(nElem);
+   if(audio==true){
+      for(int i=0;i<listActv.length;i++){
+         // Checks if the entitiy creator is the same as the name of the entity and if its audioguided
+         if(listActv[i].entityCreator == entit.getName() && listActv[i].isAudioGuided() == audio){
+            aux.addActivity(listActv[j]);
+            j++;
          }
       }
-      return aux;
    }
+   return aux;
+}
 
-   private ListOfActivities blindFiltered(Entity entit, boolean blind){
-      int j=0;
-      ListOfActivities aux = new ListOfActivities(nElem);
+  /** Method that creates a new auxiliar table to save the blind guided activities
+    * 
+    * @return a new list with the blind guied activities
+    */
+private ListOfActivities blindFiltered(Entity entit, boolean blind){
+   int j=0;
 
-      if (blind==true) {
-         for (int i=0; i < listActv.length; i++) {
-            if (listActv[i].entityCreator == entit.getName() && 
-               listActv[i].isBlindFriendly() == blind) {
-               aux.addActivity(listActv[j]);
-               j++;
-            }
+   ListOfActivities aux = new ListOfActivities(nElem);
+   if(blind==true){
+      for(int i=0;i<listActv.length;i++){
+         // Checks if the entitiy creator is the same as the name of the entity and if its blind guided
+         if(listActv[i].entityCreator == entit.getName() && listActv[i].isBlindFriendly() == blind){
+            aux.addActivity(listActv[j]);
+            j++;
          }
       }
-      return aux;
    }
+   return aux;
+}
+ /** Method that creates a new auxiliar table to save the audio & blind guided activities
+    * 
+    * @return a new list with the audio & blind guied activities
+    */
+public ListOfActivities getVisitsPerEntity(Entity entit, boolean audio, boolean blind){
 
-   public ListOfActivities getVisitsPerEntity(Entity entit, boolean audio, boolean blind){
-
-      ListOfActivities aux = new ListOfActivities(nElem);
+   ListOfActivities aux = new ListOfActivities(nElem);
    
-      aux.audioGuideFiltered(entit, audio);
-      aux.blindFiltered(entit, blind);
+   aux.audioGuideFiltered(entit, audio);
+   aux.blindFiltered(entit, blind);
    
-      return aux;
+   return aux;
    }
+}
 
    /** Method that reads from file Activity.txt
     * 
