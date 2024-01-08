8b4ecf147d860e99677698d2b62c85c34015ae9a
juancaa03 <juanca10072003@gmail.com>
2024-01-08 17:59:14 +0100

    test code

--- src/testCodeParts/ListEntitiesTest.java
+++ src/testCodeParts/ListEntitiesTest.java
@@ -5,86 +5,59 @@ import entities.ListEntities;
 
 public class ListEntitiesTest {
    public static void main(String[] args) {
-        System.out.println("ListEntities Test");
+     
+     //Creating an entity instance and checking if the data preset is correct
+     System.out.println("\n- Creating an entity instance and checking if the data preset is correct\n");
+     Entity e1 = new Entity("E1", "123456789", "1@hotmail.com");
+     System.out.println(e1);
 
-        testConstructor();
-        testGettersAndSetters();
-        testAddEntity();
-        testToString();
-        testShowEntityName();
+     //Creating a List of Entities without elements and checking it
+     System.out.println("\n- Creating a List of Entities without elements and checking it\n");
+     int size = 5;
+     ListEntities listE = new ListEntities(size);
+     System.out.println("\n" +listE);  
+
+     //Creating more Entity instances and adding them to the list
+     System.out.println("\n- Creating more Entity instances and adding them to the list\n");
+     Entity e2 = new Entity("E2", "987654321", "2@hotmail.com");
+     Entity e3 = new Entity("E3", "135724680", "3@hotmail.com");
+     Entity e4 = new Entity("Entity4", "159825362", "4@gmail.com");
+     Entity e5 = new Entity("Entity5", "603928392", "5@gmail.com");
+
+     listE.addEntity2List(e1);
+     listE.addEntity2List(e2);
+     listE.addEntity2List(e3);
+     listE.addEntity2List(e4);
+     listE.addEntity2List(e5);
+
+     //Checking if the list is correct
+     System.out.println("\n- Checking if the list is correct\n");
+     System.out.println(listE);
+
+     //Cheking setters and getters
+     System.out.println("\n- Cheking setters and getters\n");
+     //Checking different getters
+     System.out.println("\nEntity 1 -> " +e1.getName() + ", " +e1.getNumTel() + ", " +e1.getEmail());
+     System.out.println("Entity 1 -> " +listE.getEntityList()[0].getName() + ", " +listE.getEntityList()[0].getNumTel() + ", " +listE.getEntityList()[0].getEmail());
+     System.out.println("Entity 2 -> " +e2.getName() + ", " +e2.getNumTel() + ", " +e2.getEmail());
+     System.out.println("Entity 2 -> " +listE.getEntityList()[1].getName() + ", " +listE.getEntityList()[1].getNumTel() + ", " +listE.getEntityList()[1].getEmail());
+
+     //Checking setters
+     System.out.println("\n" +listE);
+     System.out.println(e1 + "\n" + e2 + "\n" + e3);
+     
+     e1.setName("Entity1");
+     e1.setEmail("1@gmail.com");
+     e2.setName("Entity2");
+     e2.setEmail("2@gmail.com");
+     e3.setName("Entity3");
+     e3.setEmail("3@gmail.com");
+     System.out.println("-> Changing names and emails...");
+     //Checking if the setters worked
+     System.out.println("\n- Checking if the setters worked\n");
+     System.out.println(listE);
+     System.out.println(e1 + "\n" + e2 + "\n" + e3);
+
    }
 
-   public static void testConstructor() {
-        System.out.println("Testing constructor");
-
-        int size = 5;
-        ListEntities listEntities = new ListEntities(size);
-    
-        // Verificar que la longitud del arreglo de entidades sea la correcta
-        System.out.println("Constructor - Length of entity list: " + (listEntities.getEntityList().length == size ? "Passed" : "Failed"));
-    
-        // Verificar que todas las posiciones del arreglo estén inicializadas a null al principio
-        boolean allNullAtStart = true;
-        for (Entity entity : listEntities.getEntityList()) {
-            if (entity != null) {
-                allNullAtStart = false;
-                break;
-            }
-        }
-        System.out.println("Constructor - All elements initialized to null at the start: " + (allNullAtStart ? "Passed" : "Failed"));
-   }
-
-   public static void testGettersAndSetters() {
-        System.out.println("Testing getters and setters");
-
-        ListEntities listEntities = new ListEntities(5);
-        int nElem = listEntities.getnElem();
-        Entity[] entityList = listEntities.getEntityList();
-
-        // Verificar que los getters devuelven los valores correctos
-        System.out.println("getnElem test: " + (nElem == 0 ? "Passed" : "Failed"));
-        System.out.println("getEntityList test: " + (entityList.length == 5 ? "Passed" : "Failed"));
-   }
-
-   public static void testAddEntity() {
-        System.out.println("Testing addEntity");
-
-        ListEntities listEntities = new ListEntities(5);
-        Entity entity1 = new Entity("Entity1", "123456789", "1@gmail.com");
-        listEntities.addEntity2List(entity1);
-
-        // Verificar que la entidad se agregó correctamente
-        System.out.println("addEntity test: " + (listEntities.getnElem() == 1 ? "Passed" : "Failed"));
-   }
-
-   public static void testToString() {
-        System.out.println("Testing toString");
-
-        ListEntities listEntities = new ListEntities(5);
-        Entity entity1 = new Entity("Entity1", "123456789", "1@gmail.com");
-        Entity entity2 = new Entity("Entity2", "987654321", "2@gmail.com");
-        listEntities.addEntity2List(entity1);
-        listEntities.addEntity2List(entity2);
-
-        // Verificar que toString devuelve la representación de cadena correcta
-        String expectedString = "Entity List => Elements: 2\n" +
-                "\n   [1] -> " + entity1 + "\n" +
-                "\n   [2] -> " + entity2 + "\n";
-        System.out.println("toString test: " + (listEntities.toString().equals(expectedString) ? "Passed" : "Failed"));
-   }
-
-   public static void testShowEntityName() {
-        System.out.println("Testing showEntityName");
-
-        ListEntities listEntities = new ListEntities(5);
-        Entity entity1 = new Entity("Entity1", "123456789", "1@gmail.com");
-        Entity entity2 = new Entity("Entity2", "987654321", "2@gmail.com");
-        listEntities.addEntity2List(entity1);
-        listEntities.addEntity2List(entity2);
-
-        // Verificar que showEntityName devuelve la representación de cadena correcta
-        String expectedString = "Available Entities: \n1. " + entity1.getName() + "\n" +
-                "2. " + entity2.getName() + "\n";
-        System.out.println("showEntityName test: " + (listEntities.showEntityName().equals(expectedString) ? "Passed" : "Failed"));
-   }
 }
